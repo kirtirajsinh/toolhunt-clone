fef9c70223e2eb89e480b9d26e16317b031f7658
alirmemon37 <alirmemona@gmail.com>
2023-11-24 18:29:36 +0530

    (chore): hero update

--- src/components/landingpage/Hero.jsx
+++ src/components/landingpage/Hero.jsx
@@ -1,12 +1,31 @@
+import Image from "next/image";
+import { Balancer } from "react-wrap-balancer";
+
 const Hero = () => {
   return (
-    <div>
-      <h1 className="text-6xl font-bold text-center mt-20">
-        Discover the best AI tools <br /> for EveryDay
-      </h1>
-      <p className="text-center mt-8">
-        Unlock productivity with 5620+ AI tools & growing
-      </p>
+    <div className="flex my-20 relative w-full lg:max-w-7xl mx-auto items-center">
+      <Image
+        src="/hero-bg1.png"
+        width={400}
+        height={320}
+        alt=""
+        className="lg:flex-1 z-10 absolute left-0 lg:static max-w-[250px] lg:max-w-full"
+      />
+      <div className="w-full lg:w-6/12 z-20">
+        <h1 className="text-5xl md:text-6xl font-bold text-center">
+          <Balancer>Discover the best AI tools for EveryDay</Balancer>
+        </h1>
+        <p className="text-center mt-8 font-sans">
+          Unlock productivity with 5620+ AI tools & growing
+        </p>
+      </div>
+      <Image
+        src="/hero-bg1.png"
+        width={400}
+        height={320}
+        alt=""
+        className="lg:flex-1 absolute right-0 lg:static rotate-180 max-w-[250px] lg:max-w-full"
+      />
     </div>
   );
 };
