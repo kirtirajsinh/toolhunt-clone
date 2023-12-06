8239271ae98d0e218ba07f9bee1c56efb921e2e1
kirtirajsinh <kirtiraj3738@gmail.com>
2023-12-06 18:21:51 +0530

    Update ShareButton and add Toast component

--- src/components/ui/ShareButton.js
+++ src/components/ui/ShareButton.js
@@ -4,8 +4,8 @@ import { BsTwitterX } from "react-icons/Bs";
 
 const ShareButton = ({ title, url }) => {
   return (
-    <div>
-      <div className="self-center text-[#11c2b6] rounded-full py-2 px-3 bg-gradient-to-r from-[#0eca90] to-[#11c2b6]  bg-clip-text  border-[1px] border-[#0eca90] hover:text-[#0eca90]">
+    <div className="w-fit">
+      <div className="self-center text-[#11c2b6]  rounded-full py-2 px-3 bg-gradient-to-r from-[#0eca90] to-[#11c2b6]  bg-clip-text  border-[1px] border-[#0eca90] hover:text-[#0eca90]">
         <TwitterShareButton
           title={title}
           url={url}
