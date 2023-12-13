5d286c3fce82a4b5ffeeb667f6bb74d316db9762
kirtirajsinh <kirtiraj3738@gmail.com>
2023-12-13 19:22:15 +0530

    Add include option to fetchTools API

--- src/pages/api/fetchTools.js
+++ src/pages/api/fetchTools.js
@@ -8,6 +8,10 @@ export default async function handler(req, res) {
     const tools = await prisma.post.findMany({
       take: 10,
       skip: 1,
+      include: {
+        postTags: true, // Include tags in the result if needed
+        postCategories: true, // Include categories in the result if needed
+      },
       cursor: { id: page },
       orderBy: { id: "asc" },
     });
