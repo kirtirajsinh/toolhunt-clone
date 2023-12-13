a9749f0e086da3431387174b5c1f13c3b59df8a7
kirtirajsinh <kirtiraj3738@gmail.com>
2023-12-13 19:23:04 +0530

    Refactor Explore.js component to handle empty tools array

--- src/components/landingpage/Explore.js
+++ src/components/landingpage/Explore.js
@@ -9,37 +9,40 @@ import { useTools } from "../hooks/tools";
 const Explore = () => {
   const router = useRouter();
   const tools = useTools((state) => state.tools);
+  console.log(tools, "tools from the hook");
 
   return (
     <section className="flex flex-col mt-12 max-w-5xl mx-auto w-full px-6 lg:px-0">
       <ExploreHeader />
       <div className="flex flex-col gap-6">
-        {tools.map(
-          ({
-            id,
-            imageUrl,
-            title,
-            rating,
-            content,
-            source,
-            featured,
-            tags,
-          }) => {
-            return (
-              <ExploreToolCard
-                key={id}
-                id={id}
-                imageUrl={imageUrl}
-                title={title}
-                rating={rating}
-                content={content}
-                featured={featured}
-                source={source}
-                tags={tags}
-              />
-            );
-          }
-        )}
+        {tools &&
+          tools.length > 0 &&
+          tools.map(
+            ({
+              id,
+              imageUrl,
+              title,
+              rating,
+              content,
+              source,
+              featured,
+              postTags,
+            }) => {
+              return (
+                <ExploreToolCard
+                  key={id}
+                  id={id}
+                  imageUrl={imageUrl}
+                  title={title}
+                  rating={rating}
+                  content={content}
+                  featured={featured}
+                  source={source}
+                  tags={postTags}
+                />
+              );
+            }
+          )}
       </div>
       {router.asPath === "/new" ? (
         <LoadMore />
