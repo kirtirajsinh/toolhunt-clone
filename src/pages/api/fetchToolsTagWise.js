66f3337563eae9aa9e258536c9eb8917c5dbebfc
kirtirajsinh <kirtiraj3738@gmail.com>
2023-12-15 22:01:25 +0530

    Add fetchToolsTagWise API endpoint

---
+++ src/pages/api/fetchToolsTagWise.js
@@ -0,0 +1,30 @@
+import { prisma } from "@/lib/prisma";
+
+export default async function handler(req, res) {
+  const { page, category } = req.query;
+  console.log(page, "page data from the query");
+  console.log(category, "category data from the query");
+  try {
+    const tools = await prisma.tag.findUnique({
+      where: { title: category },
+
+      include: {
+        Post: {
+          take: 10,
+          skip: 1,
+          cursor: { id: page },
+          orderBy: { id: "asc" },
+          include: {
+            postCategories: true,
+            postTags: true,
+          },
+        },
+      },
+    });
+    console.log("Fetched tools", tools);
+    res.status(200).json(tools);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ e });
+  }
+}
