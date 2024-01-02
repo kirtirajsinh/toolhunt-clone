86a75d2b6162c0d922b7c6216215ee30a8198163
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-02 16:50:51 +0530

    Add API endpoint to add a tool to the database

---
+++ src/pages/api/addTool.js
@@ -0,0 +1,37 @@
+import { authOptions } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { getServerSession } from "next-auth";
+
+export default async function handler(req, res) {
+  const session = await getServerSession(req, res, authOptions);
+  console.log(session, "Session");
+  if (!session) {
+    res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const { formData } = req.body;
+  console.log(formData.categories, "form data in checkout ");
+
+  try {
+    const addTool = await prisma.post.create({
+      data: {
+        title: formData.title,
+        source: formData.source,
+        authorId: session.user.id.id,
+        content: formData.content,
+        imageUrl: formData.imageUrl,
+        postCategories: {
+          connectOrCreate: formData.categories.map((categoryName) => ({
+            where: { title: categoryName },
+            create: { title: categoryName },
+          })),
+        },
+      },
+    });
+    console.log("Tool added to database", addTool);
+    res.status(200).json(addTool);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({ error: e.message });
+  }
+}
