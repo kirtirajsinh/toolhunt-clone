5626e88aee9ce28716f5cf693e24a0f7a321e058
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-02 21:28:49 +0530

    Add take parameter to limit the number of categories fetched

--- src/pages/api/getCategories.js
+++ src/pages/api/getCategories.js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
           },
         },
       },
+      take: 30,
     });
     console.log("Fetched categories", categoriesWithPostCount);
     return res.status(200).json(categoriesWithPostCount);
