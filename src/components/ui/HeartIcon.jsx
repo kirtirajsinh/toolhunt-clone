13e07105b40d8b720a579625a43539afee5e0320
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-02 16:45:33 +0530

    Add HeartIcon component & heart svg

---
+++ src/components/ui/HeartIcon.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const HeartIcon = ({ filled, width, height, className, isLiked }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill={filled}
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      dataslot="icon"
+      className={`w-6 h-6 cursor-pointer ${
+        isLiked
+          ? " hover:bg-transparent text-[#0eca90]  fill-[#0eca90] hover:scale-110 duration-100"
+          : "hover:bg-transparent text-[#0eca90] hover:fill-[#0eca90]"
+      }${className}`}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+      />
+    </svg>
+  );
+};
+
+export default HeartIcon;
