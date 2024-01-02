abe2b3390f5abfe2d96122c9ea2d21a9596f9a7e
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-02 16:47:59 +0530

    Add like and dislike functionality to Explore component

--- src/components/landingpage/Explore.js
+++ src/components/landingpage/Explore.js
@@ -5,59 +5,78 @@ import ExploreToolCard from "./ExploreToolCard";
 import { useRouter } from "next/router";
 import LoadMore from "../new/LoadMore";
 import { useTools } from "../hooks/tools";
+import { DislikePost, LikeTool } from "@/lib/utils";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import LoginButton from "../LoginButton";
 
 const Explore = () => {
   const router = useRouter();
   const tools = useTools((state) => state.tools);
-  console.log(tools, "tools from the hook");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  console.log(tools, "tools in Explore component");
 
   return (
-    <section className="flex flex-col mt-12 max-w-5xl mx-auto w-full px-6 lg:px-0">
-      <ExploreHeader />
-      <div className="flex flex-col gap-6">
-        {tools &&
-          tools.length > 0 &&
-          tools.map(
-            ({
-              id,
-              imageUrl,
-              title,
-              rating,
-              content,
-              source,
-              featured,
-              postTags,
-            }) => {
+    <>
+      <section className="flex flex-col mt-12 max-w-5xl mx-auto w-full px-6 lg:px-0">
+        <ExploreHeader />
+        <div className="flex flex-col gap-6">
+          {tools &&
+            tools.length > 0 &&
+            tools.map((tool) => {
               return (
                 <ExploreToolCard
-                  key={id}
-                  id={id}
-                  imageUrl={imageUrl}
-                  title={title}
-                  rating={rating}
-                  content={content}
-                  featured={featured}
-                  source={source}
-                  tags={postTags}
+                  key={tool.id}
+                  id={tool.id}
+                  imageUrl={tool.imageUrl}
+                  title={tool.title}
+                  rating={tool.rating}
+                  content={tool.content}
+                  featured={tool.featured}
+                  source={tool.source}
+                  tags={tool.postTags}
+                  isLiked={tool?.likes?.length > 0}
+                  setIsModalOpen={(response) => setIsModalOpen(response)}
                 />
               );
-            }
-          )}
-      </div>
-      {router.asPath === "/new" ? (
-        <LoadMore />
-      ) : (
-        <Button
-          className="self-center font-medium mt-8 mb-24 px-8 py-3 rounded-full text-base"
-          style={{
-            background: "var(--primary-button)",
-          }}
-          onClick={() => router.push("/new")}
-        >
-          Explore(5475) AI Tools
-        </Button>
-      )}
-    </section>
+            })}
+        </div>
+        {router.asPath === "/new" ? (
+          <LoadMore />
+        ) : (
+          <Button
+            className="self-center font-medium mt-8 mb-24 px-8 py-3 rounded-full text-base"
+            style={{
+              background: "var(--primary-button)",
+            }}
+            onClick={() => router.push("/new")}
+          >
+            Explore(5475) AI Tools
+          </Button>
+        )}
+      </section>
+      <Dialog
+        open={isModalOpen}
+        onOpenChange={setIsModalOpen}
+        onClose={(event) => event.preventDefault()}
+      >
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Login</DialogTitle>
+            <DialogDescription>Login to LIke any AI Tool</DialogDescription>
+          </DialogHeader>
+          <div className="flex items-center space-x-2">
+            <LoginButton />
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
--- src/components/landingpage/ExploreToolCard.jsx
+++ src/components/landingpage/ExploreToolCard.jsx
@@ -2,6 +2,12 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import Balancer from "react-wrap-balancer";
+import HeartIcon from "../ui/HeartIcon";
+import { useState } from "react";
+import { DislikePost, LikeTool } from "@/lib/utils";
+import { useSession } from "next-auth/react";
+import { Toaster } from "@/components/ui/toaster";
+import { useToast } from "@/components/ui/use-toast";
 
 const ExploreToolCard = ({
   id,
@@ -12,120 +18,136 @@ const ExploreToolCard = ({
   featured,
   content,
   tags,
+  isLiked,
+  setIsModalOpen,
 }) => {
   const router = useRouter();
-  console.log(
-    id,
-    imageUrl,
-    title,
-    source,
-    rating,
-    featured,
-    content,
-    tags,
-    "tags from the explore Card page"
-  );
+  const [likes, setIsLikes] = useState(isLiked);
+  const { toast } = useToast();
+  const { data: sessionData } = useSession();
+
+  const Like = () => {
+    if (!sessionData) {
+      setIsModalOpen(true);
+      return;
+    }
+    setIsLikes(true);
+    toast({
+      title: "Tool Liked SucessFully",
+    });
+    LikeTool(id);
+  };
+  const disLike = () => {
+    if (!sessionData) {
+      setIsModalOpen(true);
+      return;
+    }
+    setIsLikes(false);
+    toast({
+      title: "Tool Disliked SucessFully",
+    });
+    DislikePost(id);
+  };
   return (
-    <div className="w-full bg-secondary-background p-4 rounded-lg flex gap-6 relative overflow-hidden cursor-pointer flex-wrap md:flex-nowrap">
-      <div className="w-full md:max-w-[354px] md:max-h-[200px]">
-        <Image
-          src={imageUrl}
-          width={354}
-          height={185}
-          alt=""
-          className="object-cover w-full h-full"
-        />
-      </div>
-      <div className="flex flex-col w-full">
-        <div>
-          <div className="flex gap-3 items-center mb-2">
-            <h4
-              className="font-semibold text-lg  hover:text-[#0eca90] cursor-pointer"
-              onClick={() => router.push(`/tool/${id}`)}
-            >
-              {title}
-            </h4>
-            <span onClick={(e) => e.stopPropagation()}>
-              <Image
-                src="/icons/redirect.svg"
-                className="cursor-pointer"
-                width={16}
-                height={16}
-                alt={title}
-                onClick={() => window.open(source)}
-              />
-            </span>
-            <div className="flex gap-2 items-center">
-              <Image src="/icons/star.svg" width={16} height={16} alt="" />
-              <span>{rating}</span>
-            </div>
-            {featured && (
-              <div className="flex items-center py-1 px-3 bg-secondary rounded-full">
-                <span className="font-medium text-xs text-tertiary-button">
-                  Featured
-                </span>
-              </div>
-            )}
-            <div
-              className="ml-auto flex gap-2 absolute right-0 top-0 px-2 py-1"
-              style={{
-                background: "var(--primary-button)",
-              }}
-            >
-              <Image
-                src="/icons/star-circular.svg"
-                width={14}
-                height={14}
-                alt=""
-              />
-              Premium
-            </div>
-          </div>
-          <p className="font-light text-sm w-full lg:w-[80%] mb-6 text-secondary-text">
-            {content}
-          </p>
+    <>
+      <Toaster />
+      <div className="w-full bg-secondary-background p-4 rounded-lg flex gap-6 relative overflow-hidden cursor-pointer flex-wrap md:flex-nowrap">
+        <div className="w-full md:max-w-[354px] md:max-h-[200px]">
+          <Image
+            src={imageUrl}
+            width={354}
+            height={185}
+            alt=""
+            className="object-cover w-full h-full"
+          />
         </div>
-        <div className="mt-auto w-full">
-          <div className="flex flex-col sm:flex-row gap-2 justify-between">
-            {tags && (
-              <div className="flex gap-2 flex-wrap w-fit">
-                {/* show max. 8 tags */}
-                {tags?.slice(0, 4).map((tag) => {
-                  return (
-                    <Link
-                      key={tag.id}
-                      href={`/tag/${tag.title}`}
-                      className="bg-secondary rounded-sm text-tertiary-button px-2 py-1 font-light text-sm hover:bg-tertiary-button hover:text-secondary"
-                    >
-                      {tag.title}
-                    </Link>
-                  );
-                })}
-              </div>
-            )}
-            <div className="flex items-center ml-auto mt-1 self-start">
-              <div className="flex gap-1 items-center">
+        <div className="flex flex-col w-full">
+          <div>
+            <div className="flex gap-3 items-center mb-2">
+              <h4
+                className="font-semibold text-lg  hover:text-[#0eca90] cursor-pointer"
+                onClick={() => router.push(`/tool/${id}`)}
+              >
+                {title}
+              </h4>
+              <span onClick={(e) => e.stopPropagation()}>
                 <Image
-                  src="/icons/logo.svg"
+                  src="/icons/redirect.svg"
+                  className="cursor-pointer"
                   width={16}
                   height={16}
-                  alt=""
-                  className="w-4 h-4"
+                  alt={title}
+                  onClick={() => window.open(source)}
                 />
-                <span className="font-light text-sm">157.4k</span>
+              </span>
+              <div className="flex gap-2 items-center">
+                <Image src="/icons/star.svg" width={16} height={16} alt="" />
+                <span>{rating}</span>
               </div>
-              <Image
-                src="/icons/heart.svg"
-                width={16}
-                height={16}
-                alt=""
-                className="cursor-pointer w-4 h-4 ml-4"
-              />
+              {featured && (
+                <div className="flex items-center py-1 px-3 bg-secondary rounded-full">
+                  <span className="font-medium text-xs text-tertiary-button">
+                    Featured
+                  </span>
+                </div>
+              )}
+              <div
+                className="ml-auto flex gap-2 absolute right-0 top-0 px-2 py-1"
+                style={{
+                  background: "var(--primary-button)",
+                }}
+              >
+                <Image
+                  src="/icons/star-circular.svg"
+                  width={14}
+                  height={14}
+                  alt=""
+                />
+                Premium
+              </div>
+            </div>
+            <p className="font-light text-sm w-full lg:w-[80%] mb-6 text-secondary-text">
+              {content}
+            </p>
+          </div>
+          <div className="mt-auto w-full">
+            <div className="flex flex-col sm:flex-row gap-2 justify-between">
+              {tags && (
+                <div className="flex gap-2 flex-wrap w-fit">
+                  {/* show max. 8 tags */}
+                  {tags?.slice(0, 4).map((tag) => {
+                    return (
+                      <Link
+                        key={tag.id}
+                        href={`/tag/${tag.title}`}
+                        className="bg-secondary rounded-sm text-tertiary-button px-2 py-1 font-light text-sm hover:bg-tertiary-button hover:text-secondary"
+                      >
+                        {tag.title}
+                      </Link>
+                    );
+                  })}
+                </div>
+              )}
+              <div className="flex items-center ml-auto mt-1 self-start">
+                <div className="flex gap-1 items-center">
+                  <Image
+                    src="/icons/logo.svg"
+                    width={16}
+                    height={16}
+                    alt=""
+                    className="w-4 h-4"
+                  />
+                  <span className="font-light text-sm">157.4k</span>
+                </div>
+                <button onClick={likes ? disLike : Like}>
+                  <HeartIcon isLiked={likes} />
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
