7c1fbf96f74a3ab6a9be15a6cea0bcb2ab3b6e05
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-02 16:47:40 +0530

    Add LikeTool and DislikePost functions to utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -17,3 +17,41 @@ export const plans = [
     priceId: "price_1OGFf8SH5mBCFQOqMNmpMipu",
   },
 ];
+
+export async function LikeTool(toolId) {
+  try {
+    const response = await fetch("/api/like", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        toolId: toolId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data, "response from likeDislike");
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function DislikePost(toolId) {
+  try {
+    const response = await fetch("/api/dislike", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        toolId: toolId,
+      }),
+    });
+    const data = await response.json();
+    console.log(data, "response from likeDislike");
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
