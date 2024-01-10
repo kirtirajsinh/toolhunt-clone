87b3b9ea8fd0f317fd8881bda25c889c4db79960
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-10 12:16:33 +0530

    Add error handling for checkout session creation

--- src/pages/api/promote.js
+++ src/pages/api/promote.js
@@ -43,6 +43,6 @@ export default async function handler(req, res) {
     res.status(200).json(session);
   } catch (e) {
     console.log(e);
+    res.status(500).json({ error: "Error creating checkout session" });
   }
-  res.status(200).json(formData);
 }
