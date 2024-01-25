72f64def2c181cffcd2a30faf79dffba91e6b180
kirtirajsinh <kirtiraj3738@gmail.com>
2024-01-25 22:50:30 +0530

    Refactor component imports and replace icons with images

--- src/components/submit/Advantage.jsx
+++ src/components/submit/Advantage.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { CiBookmark } from "react-icons/ci";
-import { BiWorld } from "react-icons/bi";
-import { IoIosStats, IoMdPeople, IoMdSearch } from "react-icons/io";
+import Image from "next/image";
 
 const Advantage = () => {
   return (
@@ -9,24 +7,27 @@ const Advantage = () => {
       <div className="flex flex-col mt-5 space-y-8">
         <div className="flex sm:flex-row flex-col align-items-center   flex-wrap  sm:gap-4 ">
           <div className="flex flex-row items-center space-x-2 border p-2 w-full sm:w-fit ">
-            <CiBookmark />
+            <Image src="/icons/bookmark.svg" width={14} height={14} alt="" />
             <p>200,000+ subscribers</p>
           </div>
           <div className="flex flex-row items-center space-x-2 border p-2 w-full sm:w-fit ">
-            <IoMdSearch />
+            <Image src="/icons/search.svg" width={14} height={14} alt="" />
+
             <p>Do Follow BackLinks</p>
           </div>
           <div className="flex flex-row items-center space-x-2 border p-2 w-full sm:w-fit">
-            <BiWorld />
+            <Image src="/icons/world.svg" width={14} height={14} alt="" />
+
             <p>Multi Language Intro</p>
           </div>
           <div className="flex flex-row items-center space-x-2 border p-2 w-full sm:w-fit">
-            <IoIosStats />
+            <Image src="/icons/stats.svg" width={14} height={14} alt="" />
 
             <p>Listing and Traffic Forever</p>
           </div>
           <div className="flex flex-row items-center space-x-2 border p-2 w-full sm:w-fit">
-            <IoMdPeople />
+            <Image src="/icons/people.svg" width={14} height={14} alt="" />
+
             <p>Value for Founders</p>
           </div>
         </div>
