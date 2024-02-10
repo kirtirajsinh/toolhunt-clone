46fec23122c18dd523eb02c1507ed868c5a9d75e
kirtirajsinh <kirtiraj3738@gmail.com>
2024-02-10 21:30:48 +0530

    Refactor code to improve performance and readability

--- src/components/submit/SubmitTool.js
+++ src/components/submit/SubmitTool.js
@@ -7,7 +7,7 @@ import { supabase } from "@/lib/supabase";
 import { v4 as uuidv4 } from "uuid";
 import { Toaster } from "@/components/ui/toaster";
 import { useToast } from "@/components/ui/use-toast";
-
+import { useDebounce } from "@uidotdev/usehooks";
 import LoginButton from "../LoginButton";
 import {
   Dialog,
@@ -29,16 +29,25 @@ const SubmitTool = () => {
   const [loading, setLoading] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const categories = useTools((state) => state.categories);
+  const [tagsInputValue, setTagInputValue] = useState("");
+  // const categories = useTools((state) => state.categories);
+  const [tags, setTags] = useState([]);
+  const [categories, setCategories] = useState([]);
   const categoryInputRef = useRef("");
+  const tagInputRef = useRef("");
   const [showCategories, setShowCategories] = useState(false);
+  const [showTags, setShowTags] = useState(false);
   const cardRef = useRef(null);
+  const tagsCardRef = useRef(null);
+  const debouncedSearchTags = useDebounce(tagsInputValue, 500);
+  const debouncedSearchCategories = useDebounce(inputValue, 500);
   const [formData, setFormData] = React.useState({
     title: "",
     source: "",
     imageUrl: null,
     content: "",
     categories: [],
+    tags: [],
   });
 
   const mySchema = z.object({
@@ -47,23 +56,80 @@ const SubmitTool = () => {
     imageUrl: z.string().url(),
     content: z.string().min(1).max(10000),
     categories: z.array(z.string().min(1)),
+    tags: z.array(z.string().min(1)),
   });
 
-  const handleKeyDown = (event) => {
-    if (event.key === "Enter" && inputValue.trim() !== "") {
+  useEffect(() => {
+    const searchTags = async () => {
+      let results = [];
+      if (debouncedSearchCategories) {
+        const data = await fetch("/api/findCategories", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title: debouncedSearchCategories }),
+        });
+        const response = await data.json();
+        results = response;
+        console.log(response);
+      }
+
+      setCategories(results);
+    };
+
+    searchTags();
+  }, [debouncedSearchCategories, setCategories]);
+
+  useEffect(() => {
+    const searchTags = async () => {
+      let results = [];
+      if (debouncedSearchTags) {
+        const data = await fetch("/api/findTags", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ title: debouncedSearchTags }),
+        });
+        const response = await data.json();
+        results = response;
+        console.log(response);
+      }
+
+      setTags(results);
+    };
+
+    searchTags();
+  }, [debouncedSearchTags, setTags]);
+
+  const handleKeyDown = (event, variant) => {
+    console.log(variant, "variant from the handleKeyDown");
+    if (
+      event.key === "Enter" &&
+      (inputValue.trim() !== "" || tagsInputValue.trim() !== "")
+    ) {
       event.preventDefault();
       // setCategories([...categories, inputValue.trim()]);
-
+      console.log("enter pressed", inputValue);
       // add the value of the tags to the formData
-      setFormData({
-        ...formData,
-        categories: [...formData.categories, inputValue.trim()],
-      });
-      setInputValue("");
+      if (variant === "tag") {
+        setFormData({
+          ...formData,
+          tags: [...formData.tags, tagsInputValue.trim()],
+        });
+        setTagInputValue("");
+      } else if (variant === "category") {
+        setFormData({
+          ...formData,
+          categories: [...formData.categories, inputValue.trim()],
+        });
+        setInputValue("");
+      }
     }
   };
 
-  const removeTag = (indexToRemove) => {
+  const removeCategory = (indexToRemove) => {
     // setCategories([
     //   ...categories.filter((_, index) => index !== indexToRemove),
     // ]);
@@ -73,6 +139,13 @@ const SubmitTool = () => {
       categories: [
         ...formData.categories.filter((_, index) => index !== indexToRemove),
       ],
+    });
+  };
+
+  const removeTag = (indexToRemove) => {
+    setFormData({
+      ...formData,
+      tags: [...formData.tags.filter((_, index) => index !== indexToRemove)],
     });
   };
 
@@ -88,7 +161,8 @@ const SubmitTool = () => {
       !formData.source ||
       !formData.imageUrl ||
       !formData.content ||
-      !formData.categories
+      !formData.categories ||
+      !formData.tags
     ) {
       return;
     }
@@ -179,13 +253,53 @@ const SubmitTool = () => {
     });
   };
 
-  const handleClickOutside = (e) => {
+  const handleTagSelect = (tag) => {
+    const updatedTags = [...formData.tags];
+    if (updatedTags.includes(tag)) {
+      // If tag is already selected, remove it
+      updatedTags.splice(updatedTags.indexOf(tag), 1);
+    } else {
+      // If tag is not selected, add it
+      updatedTags.push(tag);
+    }
+    setFormData({
+      ...formData,
+      tags: updatedTags,
+    });
+  };
+
+  // const handleClickOutside = (event) => {
+  //   console.log(event, "event from the handleClickOutside");
+  //   if (
+  //     cardRef.current &&
+  //     !categoryInputRef.current.contains(event.target) &&
+  //     !cardRef.current.contains(event.target)
+  //   ) {
+  //     setShowCategories(false);
+  //   }
+  //   if (
+  //     tagsCardRef.current &&
+  //     tagInputRef.current.contains(event.target) &&
+  //     !tagsCardRef.current.contains(event.target)
+  //   ) {
+  //     setShowTags(false);
+  //   }
+  // };
+  const handleClickOutside = (event) => {
     if (
       cardRef.current &&
-      !categoryInputRef.current.contains(e.target) &&
-      !cardRef.current.contains(e.target)
+      !cardRef.current.contains(event.target) &&
+      !categoryInputRef.current.contains(event.target)
     ) {
       setShowCategories(false);
+    }
+
+    if (
+      tagsCardRef.current &&
+      !tagsCardRef.current.contains(event.target) &&
+      !tagInputRef.current.contains(event.target)
+    ) {
+      setShowTags(false);
     }
   };
 
@@ -235,11 +349,40 @@ const SubmitTool = () => {
             }}
             className="p-2 rounded-md border-[1px] border-[#0eca90] bg-transparent"
           ></input>
-          <label>Add Category</label>
-          {showCategories && (
+          <label className="relative">Add Category</label>
+          {formData.categories.length > 0 && (
+            <div className="flex flex-wrap gap-2 border border-gray-300 rounded-lg px-3 py-2 mt-4">
+              {formData.categories.map((tag, index) => (
+                <div
+                  key={index}
+                  className="bg-gray-200 rounded-lg flex flex-row items-center px-2 py-1 text-black"
+                >
+                  <span>{tag}</span>
+                  <button
+                    type="button"
+                    onClick={() => removeCategory(index)}
+                    className="ml-2 text-red-500 focus:outline-none"
+                  >
+                    &times;
+                  </button>
+                </div>
+              ))}
+            </div>
+          )}
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+            onKeyDown={(e) => handleKeyDown(e, "category")}
+            placeholder="Add Categories"
+            ref={categoryInputRef}
+            onFocus={() => setShowCategories(true)}
+            className="p-2 rounded-md border-[1px] border-[#0eca90] bg-transparent relative"
+          />
+          {showCategories && categories.length > 0 && (
             <Card
               ref={cardRef}
-              className="no-scrollbar flex flex-wrap gap-2 absolute inset-x-6 bottom-10    border rounded-lg px-3 py-2  h-32 overflow-scroll bg-secondary-background rounded shadow-md"
+              className="no-scrollbar z-10 flex flex-wrap gap-2 absolute inset-x-6 bottom-32 md:bottom-36    border rounded-lg px-3 py-2  h-32 overflow-scroll bg-secondary-background rounded shadow-md"
             >
               {categories.map((category) => (
                 <div key={category.id} className="space-x-2">
@@ -254,9 +397,11 @@ const SubmitTool = () => {
               ))}
             </Card>
           )}
-          {formData.categories.length > 0 && (
+          <label>Add Tags</label>
+
+          {formData.tags.length > 0 && (
             <div className="flex flex-wrap gap-2 border border-gray-300 rounded-lg px-3 py-2 mt-4">
-              {formData.categories.map((tag, index) => (
+              {formData.tags.map((tag, index) => (
                 <div
                   key={index}
                   className="bg-gray-200 rounded-lg flex flex-row items-center px-2 py-1 text-black"
@@ -275,14 +420,32 @@ const SubmitTool = () => {
           )}
           <input
             type="text"
-            value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
-            onKeyDown={handleKeyDown}
-            placeholder="Add Categories"
-            ref={categoryInputRef}
-            onFocus={() => setShowCategories(true)}
-            className="p-2 rounded-md border-[1px] border-[#0eca90] bg-transparent "
+            value={tagsInputValue}
+            onChange={(e) => setTagInputValue(e.target.value)}
+            onKeyDown={(e) => handleKeyDown(e, "tag")}
+            placeholder="Add Tags"
+            ref={tagInputRef}
+            onFocus={() => setShowTags(true)}
+            className="p-2 rounded-md border-[1px] border-[#0eca90] bg-transparent relative"
           />
+          {showTags && tags.length > 0 && (
+            <Card
+              ref={tagsCardRef}
+              className="no-scrollbar z-10 flex flex-wrap gap-2 absolute inset-x-6 bottom-4 md:bottom-10    border rounded-lg px-3 py-2  h-32 overflow-scroll bg-secondary-background rounded shadow-md"
+            >
+              {tags.map((tag) => (
+                <div key={tag.id} className="space-x-2">
+                  <input
+                    type="checkbox"
+                    id={tag.id}
+                    checked={formData.tags.includes(tag.title)}
+                    onChange={() => handleTagSelect(tag.title)}
+                  />
+                  <label htmlFor={tag.title}>{tag.title}</label>
+                </div>
+              ))}
+            </Card>
+          )}
           <label>
             Add Image:
             <input
